aa14c344220f2be51fe828cbf0c297c3c063d0dd
Pirulax <patrikjankovics7@gmail.com>
2020-06-22 04:15:38 +0200

    Reorder decodeXtea to be above TeaDecode

--- Shared/sdk/SharedUtil.Hash.hpp
+++ Shared/sdk/SharedUtil.Hash.hpp
@@ -767,6 +767,19 @@ namespace SharedUtil
 
         return true;
     }
+
+    void decodeXtea(unsigned int* v, unsigned int* w, unsigned int* k)
+    {
+        unsigned int v0 = v[0], v1 = v[1], i, sum = 0xC6EF3720;
+        unsigned int delta = 0x9E3779B9;
+        for (i = 0; i < 32; i++)
+        {
+            v1 -= (((v0 << 4) ^ (v0 >> 5)) + v0) ^ (sum + k[(sum >> 11) & 3]);
+            sum -= delta;
+            v0 -= (((v1 << 4) ^ (v1 >> 5)) + v1) ^ (sum + k[sum & 3]);
+        }
+        w[0] = v0;
+        w[1] = v1;
     }
 
     void TeaDecode(const SString& str, const SString& key, SString* out)
