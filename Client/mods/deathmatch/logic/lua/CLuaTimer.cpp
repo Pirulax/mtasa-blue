603ff2f3e1842cfbef42f8bf1428fed08423a0e3
Pirulax <patrikjankovics7@gmail.com>
2020-11-17 03:33:44 +0100

    Refactor CLuaTimer

--- Client/mods/deathmatch/logic/lua/CLuaTimer.cpp
+++ Client/mods/deathmatch/logic/lua/CLuaTimer.cpp
@@ -11,12 +11,14 @@
 
 #include <StdInc.h>
 
-CLuaTimer::CLuaTimer(const CLuaFunctionRef& iLuaFunction, const CLuaArguments& Arguments)
+CLuaTimer::CLuaTimer(const CLuaFunctionRef& iLuaFunction, CLuaArguments Arguments, CTickCount PulseInterval, size_t Repeats) :
+    m_uiScriptID(CIdArray::PopUniqueId(this, EIdClass::TIMER)),
+    m_uiRepeats(Repeats),
+    m_PulseInterval(PulseInterval),
+    m_NextPulseTick(CTickCount::Now() + PulseInterval), // Default init
+    m_iLuaFunction(iLuaFunction),
+    m_Arguments(std::move(Arguments))
 {
-    m_uiScriptID = CIdArray::PopUniqueId(this, EIdClass::TIMER);
-    m_uiRepeats = 1;
-    m_iLuaFunction = iLuaFunction;
-    m_Arguments = Arguments;
 }
 
 CLuaTimer::~CLuaTimer()
@@ -24,7 +26,7 @@ CLuaTimer::~CLuaTimer()
     RemoveScriptID();
 }
 
-void CLuaTimer::RemoveScriptID()
+void CLuaTimer::RemoveScriptID() noexcept
 {
     if (m_uiScriptID != INVALID_ARRAY_ID)
     {
@@ -33,34 +35,68 @@ void CLuaTimer::RemoveScriptID()
     }
 }
 
-void CLuaTimer::ExecuteTimer(CLuaMain* pLuaMain)
+void CLuaTimer::SetPendingDelete()
 {
-    if (VERIFY_FUNCTION(m_iLuaFunction))
-    {
-        lua_State* pState = pLuaMain->GetVM();
+    dassert(!IsPendingDelete());
 
-        LUA_CHECKSTACK(pState, 1);            // Ensure some room
-
-        // Store the current values of the globals
-        lua_getglobal(pState, "sourceTimer");
-        CLuaArgument OldSource(pState, -1);
-        lua_pop(pState, 1);
-
-        // Set the "sourceTimer" global
-        lua_pushtimer(pState, this);
-        lua_setglobal(pState, "sourceTimer");
-
-        m_Arguments.Call(pLuaMain, m_iLuaFunction);
-
-        // Reset the globals on that VM
-        OldSource.Push(pState);
-        lua_setglobal(pState, "sourceTimer");
-    }
+    RemoveScriptID();
+    m_status = Status::PENDING_DELETE;
 }
 
-CTickCount CLuaTimer::GetTimeLeft()
+bool CLuaTimer::Pulse(CTickCount timeNow, CLuaMain* pLuaMain)
 {
-    CTickCount llCurrentTime = CTickCount::Now();
-    CTickCount llTimeLeft = m_llStartTime + m_llDelay - llCurrentTime;
-    return llTimeLeft;
+    /* Checked by the manager
+    if (m_status == Status::PENDING_DELETE)
+        return false;
+    */ 
+
+    if (m_NextPulseTick > timeNow)
+        return false;
+
+    if (!VERIFY_FUNCTION(m_iLuaFunction))
+        return false; // Perhaps `SetPendingDelete()` here?
+
+    // Actually process this timer (call the Lua function)
+    {
+        // Save debug info, so its reported correctly
+        g_pClientGame->GetScriptDebugging()->SaveLuaDebugInfo(GetLuaDebugInfo());
+
+        // Grab the main state
+        // TODO: Check if coroutines work properly if its like this..
+        lua_State* pLuaMainState = pLuaMain->GetVM();
+
+        // Ensure some room
+        LUA_CHECKSTACK(pLuaMainState, 1);
+
+        // Store the current value of the sourceTimer global
+        lua_getglobal(pLuaMainState, "sourceTimer");
+        CLuaArgument OldSource(pLuaMainState, -1);
+        lua_pop(pLuaMainState, 1);
+
+        // Set the "sourceTimer" global
+        lua_pushtimer(pLuaMainState, this);
+        lua_setglobal(pLuaMainState, "sourceTimer");
+
+        // Call the handler Lua function
+        m_Arguments.Call(pLuaMain, m_iLuaFunction);
+
+        // Reset the sourceTimer global
+        OldSource.Push(pLuaMainState);
+        lua_setglobal(pLuaMainState, "sourceTimer");
+
+        // Reset the debug info it
+        g_pClientGame->GetScriptDebugging()->SaveLuaDebugInfo({});
+    }
+
+    if (m_uiRepeats != 0) // Not infinite repeats?
+    {
+        m_uiRepeats--;
+        if (!m_uiRepeats)
+            SetPendingDelete(); // No more repeats
+    }
+
+    if (!IsPendingDelete())
+        m_NextPulseTick = timeNow + m_PulseInterval;
+
+    return true;
 }
--- Client/mods/deathmatch/logic/lua/CLuaTimer.h
+++ Client/mods/deathmatch/logic/lua/CLuaTimer.h
@@ -19,37 +19,66 @@ class CLuaTimer;
 
 #define LUA_TIMER_MIN_INTERVAL      0
 
+
 class CLuaTimer
 {
+    friend class CLuaTimerManager;
+
+    enum class PulseStatus : uint8_t
+    {
+        OK,
+        SKIPPED,                    // When Status::SKIP_ONCE
+        NOT_REACHED_PULSE_INTERVAL, // When m_NextPulseTick < now
+        PENDING_DELETE,             // When Status::PENDING_DELETE
+        INVALID_FUNCTION            // When VERIFY_FUNCTIOn fails
+    };
+
+    enum class Status : uint8_t
+    {
+        SKIP_ONCE,
+        PENDING_DELETE,
+        REPEATING
+    };
 public:
-    CLuaTimer(const CLuaFunctionRef& iLuaFunction, const CLuaArguments& Arguments);
+    CLuaTimer(const CLuaFunctionRef& iLuaFunction, CLuaArguments Arguments, CTickCount PulseInterval, size_t Repeats);
     ~CLuaTimer();
 
-    void RemoveScriptID();
+    uint GetScriptID() const { return m_uiScriptID; }
 
-    CTickCount GetStartTime() const { return m_llStartTime; };
-    void       SetStartTime(CTickCount llStartTime) { m_llStartTime = llStartTime; };
-
-    CTickCount GetDelay() const { return m_llDelay; };
-    void       SetDelay(CTickCount llDelay) { m_llDelay = llDelay; };
-
-    unsigned int GetRepeats() const { return m_uiRepeats; };
-    void         SetRepeats(unsigned int uiRepeats) { m_uiRepeats = uiRepeats; }
-
-    void ExecuteTimer(class CLuaMain* pLuaMain);
-
-    CTickCount GetTimeLeft();
-
-    uint                 GetScriptID() const { return m_uiScriptID; }
     const SLuaDebugInfo& GetLuaDebugInfo() { return m_LuaDebugInfo; }
     void                 SetLuaDebugInfo(const SLuaDebugInfo& luaDebugInfo) { m_LuaDebugInfo = luaDebugInfo; }
+
+    CTickCount GetNextPulseTick() const noexcept { return m_NextPulseTick; }
+    CTickCount GetTimeLeft() const noexcept { return m_NextPulseTick - CTickCount::Now(); }
+    CTickCount GetDelay() const noexcept { return m_PulseInterval; }
+
+    CTickCount GetTimeLeft(CTickCount now = CTickCount::Now()) const noexcept { return m_NextPulseTick - now; }
+
+    Status GetStatus() const noexcept { return m_status; }
+
+    bool IsPendingDelete() const noexcept { return m_status == Status::PENDING_DELETE; }
+    void SetPendingDelete();
+
+    bool Pulse(CTickCount timeNow, class CLuaMain* pLuaMain);
+
+    void Reset() noexcept
+    {
+        if(!IsPendingDelete())
+            m_NextPulseTick = CTickCount::Now();
+    }
+
+private:
+    void RemoveScriptID() noexcept;
 
 private:
     CLuaFunctionRef m_iLuaFunction;
     CLuaArguments   m_Arguments;
-    CTickCount      m_llStartTime;
-    CTickCount      m_llDelay;
-    unsigned int    m_uiRepeats;
-    uint            m_uiScriptID;
     SLuaDebugInfo   m_LuaDebugInfo;
+    uint            m_uiScriptID;
+
+    size_t          m_uiRepeats;
+    CTickCount      m_PulseInterval;
+    CTickCount      m_NextPulseTick;
+
+    Status          m_status;
 };
